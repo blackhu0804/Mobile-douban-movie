81f957f0b810f40e3a8ca9dd92f5a0d084a9e992
hu970804 <812510003@qq.com>
2017-10-29 11:23:20 +0800

    抽离一些组件并且修复bug

--- js/js.js
+++ js/js.js
@@ -1,18 +1,68 @@
+var Helper = {
+	isToEnd: function($viewport, $content) {
+		return $viewport.height() + $viewport.scrollTop() + 10 >= $content.height();
+	},
+	creatNode: function(movie) {
+		var tpl = 
+		`
+		<div class="item">
+			<a href="#">
+				<div class="cover">
+					<img src="http://img7.doubanio.com/img/celebrity/small/17525.jpg" alt="">          
+				</div>
+				<div class="detail">
+					<h2>霸王别姬</h2>
+					<div class="extra"><span class="score">9.3分</span> / <span class="collect"></span>收藏</div>
+					<div class="extra"><span class="year"></span> / <span class="type"></span></div>
+					<div class="extra">导演：<span class="director"></span></div>
+					<div class="extra">主演：<span class="actor"></span></div>
+				</div>
+			</a>
+		</div>
+		`
+		var $node = $(tpl);
+		$node.find('a').attr('href', movie.alt);
+		$node.find('.cover img').attr('src',movie.images.medium);
+		$node.find('.detail h2').text(movie.title);
+		$node.find('.detail .score').text(movie.rating.average);
+		$node.find('.detail .collect').text(movie.collect_count);
+		$node.find('.detail .year').text(movie.year);
+		$node.find('.detail .type').text(movie.genres.join('/'))
+		$node.find('.director').text(function() {
+			var directorsArr = [];
+			movie.directors.forEach(function(item) {
+				directorsArr.push(item.name);
+			})
+			return directorsArr.join('、');
+		})
+		$node.find('.actor').text(function() {
+			var actorArr = [];
+			movie.casts.forEach(function(item) {
+				actorArr.push(item.name);
+			})
+			return actorArr.join('、');
+		})
+		return $node;
+	}
+}
+
+
 var top250 = {
 	init : function() {
-		this.$element = $('#top250');
-		this.isLoading = false;
+		this.$container = $('#top250');
+		this.$content = this.$container.find('.container');
 		this.index = 0;
+		this.isLoading = false;
 		this.isFinish = false;
-		if(!this.isFinish) {
-			this.bind();
-			this.start();
-		}
+		this.bind();
+		this.start();
 	},
 	bind : function() {
 		var self = this;
-		this.$element.scroll(function() {
-			self.start()
+		this.$container.scroll(function() {
+			if(!self.isFinish && Helper.isToEnd(self.$container, self.$content)){
+				self.start();	
+			}
 		})
 	},
 	start : function() {
@@ -26,7 +76,7 @@ var top250 = {
 		if(self.isLoading)
 			return;
 		self.isLoading = true;
-		self.$element.find('.loading').show();
+		self.$container.find('.loading').show();
 		$.ajax({
 			url: 'http://api.douban.com/v2/movie/top250',
 			type: 'GET',
@@ -34,75 +84,32 @@ var top250 = {
 				start: self.index,
 				count: 20
 			},
-			header:{
-        "Content-Type":"application/json"
-    	},
 			dataType: 'jsonp'
 		}).done(function(ret){
-			self.render(ret);
 			self.index += 20;
 			if(self.index >= ret.total){
 				self.isFinish = true;
 			}
+			callback&&callback(ret);
 		}).fail(function(){
 			console.log('数据异常')
 		}).always(function() {
 			self.isLoading = false;
-			self.$element.find('.loading').hide();
+			self.$container.find('.loading').hide();
 		})
 	},
 	render : function(data) {
 		var self = this;
 		data.subjects.forEach(function(movie) {
-			var tpl = 
-				`
-				<div class="item">
-					<a href="#">
-						<div class="cover">
-							<img src="http://img7.doubanio.com/img/celebrity/small/17525.jpg" alt="">          
-						</div>
-						<div class="detail">
-							<h2>霸王别姬</h2>
-							<div class="extra"><span class="score">9.3分</span> / <span class="collect"></span>收藏</div>
-							<div class="extra"><span class="year"></span> / <span class="type"></span></div>
-							<div class="extra">导演：<span class="director"></span></div>
-							<div class="extra">主演：<span class="actor"></span></div>
-						</div>
-					</a>
-				</div>
-				`
-			var $node = $(tpl);
-			$node.find('.cover img').attr('src',movie.images.medium);
-			$node.find('.detail h2').text(movie.title);
-			$node.find('.detail .score').text(movie.rating.average);
-			$node.find('.detail .collect').text(movie.collect_count);
-			$node.find('.detail .year').text(movie.year);
-			$node.find('.detail .type').text(movie.genres.join('/'))
-			$node.find('.director').text(function() {
-				var directorsArr = [];
-				movie.directors.forEach(function(item) {
-					directorsArr.push(item.name);
-				})
-				return directorsArr.join('、');
-			})
-			$node.find('.actor').text(function() {
-				var actorArr = [];
-				movie.casts.forEach(function(item) {
-					actorArr.push(item.name);
-				})
-				return actorArr.join('、');
-			})
-			self.$element.find('.container').append($node);
+			self.$content.append(Helper.creatNode(movie));
 		})
-	},
-	isToBottom: function() {
-		return this.$element.find('.container') <= this.$element.height() + this.$element.scrollTop() + 10;
 	}
 }
 
 var usBox = {
 	init : function() {
-		this.$element = $('#beimei');
+		this.$container = $('#beimei');
+		this.$content = this.$container.find('.container');
 		this.start();
 	},
 	start : function() {
@@ -113,84 +120,39 @@ var usBox = {
 	},
 	getData : function(callback) {
 		var self = this;
-		self.$element.find('.loading').show();
 		$.ajax({
 			url: 'http://api.douban.com/v2/movie/us_box',
 			type: 'GET',
-			header:{
-        "Content-Type":"application/json"
-    	},
 			dataType: 'jsonp'
 		}).done(function(ret){
-			self.render(ret);
+			callback && callback(ret);
 		}).fail(function(){
 			console.log('数据异常')
 		}).always(function() {
-			self.$element.find('.loading').hide();
+			
 		})
 	},
 	render : function(data) {
 		console.log(data)
 		var self = this;
 		data.subjects.forEach(function(movie) {
-			movie = movie.subject;
-			var tpl = 
-				`
-				<div class="item">
-					<a href="#">
-						<div class="cover">
-							<img src="http://img7.doubanio.com/img/celebrity/small/17525.jpg" alt="">          
-						</div>
-						<div class="detail">
-							<h2></h2>
-							<div class="extra"><span class="score">9.3分</span> / <span class="collect"></span>收藏</div>
-							<div class="extra"><span class="year"></span> / <span class="type"></span></div>
-							<div class="extra">导演：<span class="director"></span></div>
-							<div class="extra">主演：<span class="actor"></span></div>
-						</div>
-					</a>
-				</div>
-				`
-			var $node = $(tpl);
-			$node.find('.cover img').attr('src',movie.images.medium);
-			$node.find('.detail h2').text(movie.title);
-			$node.find('.detail .score').text(movie.rating.average);
-			$node.find('.detail .collect').text(movie.collect_count);
-			$node.find('.detail .year').text(movie.year);
-			$node.find('.detail .type').text(movie.genres.join('/'))
-			$node.find('.director').text(function() {
-				var directorsArr = [];
-				movie.directors.forEach(function(item) {
-					directorsArr.push(item.name);
-				})
-				return directorsArr.join('、');
-			})
-			$node.find('.actor').text(function() {
-				var actorArr = [];
-				movie.casts.forEach(function(item) {
-					actorArr.push(item.name);
-				})
-				return actorArr.join('、');
-			})
-			self.$element.find('.container').append($node);
+			self.$content.append(Helper.creatNode(movie.subject));
 		})
-	},
-	isToBottom: function() {
-		return this.$element.find('.container') <= this.$element.height() + this.$element.scrollTop() + 10;
 	}
 }
 
 var search = {
 	init : function() {
-		this.$element = $('#search');
+		this.$container = $('#search');
+		this.$content = this.$container.find('.search-result');
 		this.keyword = '';
 		this.bind()
 		this.start();
 	},
 	bind : function() {
 		var self = this;
-		this.$element.find('.button').click(function() {
-			self.keyword = self.$element.find('input').val();
+		this.$container.find('.button').click(function() {
+			self.keyword = self.$container.find('input').val();
 			self.start();
 		})
 	},
@@ -202,7 +164,7 @@ var search = {
 	},
 	getData : function(callback) {
 		var self = this;
-		self.$element.find('.loading').show();
+		self.$container.find('.loading').show();
 		$.ajax({
 			url: 'http://api.douban.com/v2/movie/search',
 			type: 'GET',
@@ -211,60 +173,19 @@ var search = {
 			},
 			dataType: 'jsonp'
 		}).done(function(ret){
-			self.render(ret);
+			callback && callback(ret)
 		}).fail(function(){
 			console.log('数据异常')
 		}).always(function() {
-			self.$element.find('.loading').hide();
+			self.$container.find('.loading').hide();
 		})
 	},
 	render : function(data) {
-		console.log(data)
 		var self = this;
+		console.log(data)
 		data.subjects.forEach(function(movie) {
-			var tpl = 
-				`
-				<div class="item">
-					<a href="#">
-						<div class="cover">
-							<img src="http://img7.doubanio.com/img/celebrity/small/17525.jpg" alt="">          
-						</div>
-						<div class="detail">
-							<h2></h2>
-							<div class="extra"><span class="score">9.3</span>分 / <span class="collect"></span>收藏</div>
-							<div class="extra"><span class="year"></span> / <span class="type"></span></div>
-							<div class="extra">导演：<span class="director"></span></div>
-							<div class="extra">主演：<span class="actor"></span></div>
-						</div>
-					</a>
-				</div>
-				`
-			var $node = $(tpl);
-			$node.find('.cover img').attr('src',movie.images.medium);
-			$node.find('.detail h2').text(movie.title);
-			$node.find('.detail .score').text(movie.rating.average);
-			$node.find('.detail .collect').text(movie.collect_count);
-			$node.find('.detail .year').text(movie.year);
-			$node.find('.detail .type').text(movie.genres.join('/'))
-			$node.find('.director').text(function() {
-				var directorsArr = [];
-				movie.directors.forEach(function(item) {
-					directorsArr.push(item.name);
-				})
-				return directorsArr.join('、');
-			})
-			$node.find('.actor').text(function() {
-				var actorArr = [];
-				movie.casts.forEach(function(item) {
-					actorArr.push(item.name);
-				})
-				return actorArr.join('、');
-			})
-			self.$element.find('.search-result').append($node);
+			self.$content.append(Helper.creatNode(movie));
 		})
-	},
-	isToBottom: function() {
-		return this.$element.find('.container') <= this.$element.height() + this.$element.scrollTop() + 10;
 	}
 }
 
