7853cdbbd86f691eeba157b14aa904535f51a7b2
hu970804 <812510003@qq.com>
2017-11-06 20:34:36 +0800

    用webpack 自动引入jquery

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -8,12 +9,37 @@ module.exports = {
     filename: 'bundle.js', //打包后输出的文件名
     publicPath: 'build/' // 打包后的文件夹
   },
+  resolve: {
+    alias: {
+      jquery: path.join(__dirname, 'src/lib/jquery.min.js')
+    }
+  },
+  plugins: [
+    new ExtractTextPlugin("css/[name].css"),
+    new webpack.ProvidePlugin({
+      $: "jquery"
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: /(node_modules)/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|woff|woff2|ttf|eot|svg|swf)$/,
+        loader: "file-loader?name=[name]_[sha512:hash:base64:7].[ext]",
+        options: {
+          name: 'assets/[name]_[sha512:hash:base64:7].[ext]'
+        }
       }
     ]
   }
